c03b507b13414c0f48812554d905d8267bb44f68
Steve Hocking <steve@codemunki.es>
2020-11-22 22:23:42 +0000

    Add failing test for valid coin in coin collector

---
+++ tests/VendingMachine.Tests/Mechanism/CoinCollectorTests.cs
@@ -0,0 +1,23 @@
+using FluentAssertions;
+using VendingMachine.MachineInterface;
+using VendingMachine.Mechanism;
+using Xunit;
+
+namespace VendingMachine.Tests.Mechanism
+{
+    public class CoinCollectorTests
+    {
+        [Fact]
+        public void GivenThatTheCoinIAmAddingIsValid_ThenItIsAccepted()
+        {
+            // Given
+            ICollectCoins subject = new CoinCollector();
+
+            // When
+            var result = subject.Add(new Coin(2268, 705)); // Dime
+
+            // Then
+            result.Should().BeTrue();
+        }
+    }
+}
