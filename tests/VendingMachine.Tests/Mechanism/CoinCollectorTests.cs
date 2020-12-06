740241cf2bdd3446313fb87d58b06920965f91bd
Steve Hocking <steve@codemunki.es>
2020-12-06 08:56:49 +0000

    Add test for coin collector resetting itself after successful checkout

--- tests/VendingMachine.Tests/Mechanism/CoinCollectorTests.cs
+++ tests/VendingMachine.Tests/Mechanism/CoinCollectorTests.cs
@@ -120,5 +120,30 @@ namespace VendingMachine.Tests.Mechanism
             subject.Add(coin).Should().BeTrue();
             subject.Checkout(checkoutValue).Should().BeTrue();
         }
+
+        [Fact]
+        public void GivenThatASuccessfulCheckoutHasOccurred_WhenIAttemptASecondCheckoutWithoutInsertingMoreCoins_ThenFalseIsReturned()
+        {
+            // Given
+            const int checkoutValue = 50;
+            var coin = new Coin(2268, 17910); // Dime
+            var validatorMock1 = new Mock<IEvaluateCoin>();
+            var validatorMock2 = new Mock<IEvaluateCoin>();
+            ICollectCoins subject = new CoinCollector(new [] { validatorMock1.Object, validatorMock2.Object });
+
+
+            validatorMock1.Setup(v => v.Validate(coin)).Returns(false);
+            validatorMock1.SetupGet(v => v.CoinValue).Returns(checkoutValue + 1);
+            validatorMock2.Setup(v => v.Validate(coin)).Returns(true);
+            validatorMock2.SetupGet(v => v.CoinValue).Returns(checkoutValue);
+
+            // When
+            // Then - checkout one
+            subject.Add(coin).Should().BeTrue();
+            subject.Checkout(checkoutValue).Should().BeTrue();
+
+            // Then - checkout two
+            subject.Checkout(checkoutValue).Should().BeFalse();
+        }
     }
 }
